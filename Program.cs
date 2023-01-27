9f4a3be83eccce3431f973477fc9881e067f3b7a
Snehasrg <sghongade642@gmail.com>
2023-01-27 22:53:22 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace IndianStatesCensusAnalyzerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Indian States Census Analyser program  ");
+
+        }
+    }
+}
